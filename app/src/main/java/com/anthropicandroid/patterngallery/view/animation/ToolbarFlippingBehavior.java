a1812ad4c2240ebc3e5774a8ba11f9bb51abcb86
Andrew Brin <anthropic.android@gmail.com>
2017-05-21 16:18:11 -0700

    first cut; making new layout

--- app/src/main/java/com/anthropicandroid/patterngallery/view/animation/ToolbarFlippingBehavior.java
+++ app/src/main/java/com/anthropicandroid/patterngallery/view/animation/ToolbarFlippingBehavior.java
@@ -7,6 +7,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.anthropicandroid.patterngallery.databinding.LayoutActivityGalleryBinding;
 import com.anthropicandroid.patterngallery.routers.gallery.GalleryActivityComponent;
@@ -38,7 +39,7 @@ public class ToolbarFlippingBehavior
             LinearLayout layout,
             View dependency
     ) {
-        return dependency instanceof FrameLayout;
+        return dependency instanceof RelativeLayout;
     }
 
     @Override
