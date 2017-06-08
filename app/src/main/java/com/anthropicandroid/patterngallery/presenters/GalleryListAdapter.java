295dfec1ea8afc97c1c072782e09444b5175182a
Andrew Brin <anthropic.android@gmail.com>
2017-06-07 18:11:08 -0700

    added patterns list to adapter
      - adapter supplies it's own list
      - list does not seem to need to be more than private right now

--- app/src/main/java/com/anthropicandroid/patterngallery/presenters/GalleryListAdapter.java
+++ app/src/main/java/com/anthropicandroid/patterngallery/presenters/GalleryListAdapter.java
@@ -7,24 +7,24 @@ package com.anthropicandroid.patterngallery.presenters;
 import android.content.res.TypedArray;
 import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
-import android.databinding.ViewDataBinding;
 import android.support.v7.util.SortedList;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.anthropicandroid.patterngallery.R;
-import com.anthropicandroid.patterngallery.databinding.LayoutActivityGalleryBinding;
 import com.anthropicandroid.patterngallery.databinding.LayoutGalleryImageBinding;
 import com.anthropicandroid.patterngallery.entities.interactions.PatternMetaData;
 import com.anthropicandroid.patterngallery.entities.ui.GalleryItemViewModel;
-import com.anthropicandroid.patterngallery.interactors.PatternRepository;
 import com.anthropicandroid.patterngallery.routers.gallery.GalleryActivityComponent;
 import com.anthropicandroid.patterngallery.entities.ui.RawBitmapMeasurement;
 
-import java.util.ArrayList;
-import java.util.List;
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
@@ -32,20 +32,8 @@ public class GalleryListAdapter
         extends RecyclerView.Adapter {
 
     public static final String TAG = GalleryListAdapter.class.getSimpleName();
-    private GalleryActionHandlers galleryActionHandlers;
-    private int childWidth;
-    private TypedArray bgColors;
 
-    public GalleryListAdapter(
-            GalleryActionHandlers galleryActionHandlers,
-            int childWidth,
-            TypedArray bgColors
-    ) {
-        this.galleryActionHandlers = galleryActionHandlers;
-        // class constructed by an annotated static function
-        this.childWidth = childWidth;
-        this.bgColors = bgColors;
-    }
+    private static Subscription patternSubscription;
 
     @BindingAdapter(value = {
             "numSpans",
@@ -56,13 +44,126 @@ public class GalleryListAdapter
             Integer numSpans,
             String patternSet
     ) {
-//        create a new sortedarrylist
-//        ((LayoutActivityGalleryBinding) DataBindingUtil.findBinding(view)).getSvgGalleryViewModel().setPatterns();
         // create and populate list adapter and give it to the view
         view.setAdapter(new GalleryListAdapter(
                 galleryActivityComponent.getGalleryActionHandlers(),
+                galleryActivityComponent.getRepository().observePatterns(patternGroup),
                 galleryActivityComponent.getNarrowestScreenDimenInPx() / numSpans,
                 view.getContext().getResources().obtainTypedArray(R.array.bg_colors)));
+
+        view.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+            @Override
+            public void onViewAttachedToWindow(View view) {
+                Log.d(TAG, "I think the program will never get here");
+            }
+
+            @Override
+            public void onViewDetachedFromWindow(
+                    View view
+            ) {
+                if (patternSubscription != null && !patternSubscription.isUnsubscribed()) patternSubscription
+                        .unsubscribe();
+            }
+        });
+    }
+
+
+    private GalleryActionHandlers galleryActionHandlers;
+    private int childWidth;
+    private TypedArray bgColors;
+    private SortedList<PatternMetaData> patterns = new SortedList<>(
+            PatternMetaData.class,
+            new SortedList.Callback<PatternMetaData>() {
+                @Override
+                public int compare(
+                        PatternMetaData o1,
+                        PatternMetaData o2
+                ) {
+                    return 0;
+                }
+
+                @Override
+                public void onInserted(
+                        int position,
+                        int count
+                ) {
+                    onInserted(position, count);
+                }
+
+                @Override
+                public void onRemoved(
+                        int position,
+                        int count
+                ) {
+                    onRemoved(position, count);
+                }
+
+                @Override
+                public void onMoved(
+                        int fromPosition,
+                        int toPosition
+                ) {
+                    onMoved(fromPosition, toPosition);
+                }
+
+                @Override
+                public void onChanged(
+                        int position,
+                        int count
+                ) {
+                    onChanged(position, count);
+                }
+
+                @Override
+                public boolean areContentsTheSame(
+                        PatternMetaData oldItem,
+                        PatternMetaData newItem
+                ) {
+                    return oldItem.equals(newItem);
+                }
+
+                @Override
+                public boolean areItemsTheSame(
+                        PatternMetaData item1,
+                        PatternMetaData item2
+                ) {
+                    return item1.getOriginalUri().equals(item2.getOriginalUri());
+                }
+            },
+            8);
+
+    public GalleryListAdapter(
+            GalleryActionHandlers galleryActionHandlers,
+            Observable<PatternMetaData> patternObservable,
+            int childWidth,
+            TypedArray bgColors
+    ) {
+        // class constructed by an annotated static function
+
+        this.galleryActionHandlers = galleryActionHandlers;
+        this.childWidth = childWidth;
+        this.bgColors = bgColors;
+
+        patternSubscription = patternObservable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<PatternMetaData>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "pattern observable completed");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "Error while presenting patterns: " + e.getMessage());
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(PatternMetaData patternMetaData) {
+                        patterns.add(patternMetaData);
+                        Log.d(TAG, "gallery list adapter");
+                    }
+                });
     }
 
     public static class BindingHolder
@@ -114,14 +215,12 @@ public class GalleryListAdapter
             galleryItemViewModel.setRawBitmapMeasurement(new RawBitmapMeasurement());
             galleryItemViewModel.setWidth(childWidth);
             galleryItemViewModel.setDescription("Photo " + position);
-            galleryItemViewModel.setColorResId(bgColors
-                                                       .getResourceId(position % 8, R.color.colorOrange));
+            galleryItemViewModel.setColorResId(bgColors.getResourceId(position % 8, R.color.colorOrange));
         }
     }
 
     @Override
     public int getItemCount() {
-//        return imageIndicies.size();
-        return 8;
+        return patterns.size();
     }
 }
