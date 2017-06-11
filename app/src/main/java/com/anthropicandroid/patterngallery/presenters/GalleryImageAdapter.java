a80471d6cf263db6635edaf2942e37a4e0ab3426
Andrew Brin <anthropic.android@gmail.com>
2017-06-11 04:20:42 -0700

    code cleanup -- import

--- app/src/main/java/com/anthropicandroid/patterngallery/presenters/GalleryImageAdapter.java
+++ app/src/main/java/com/anthropicandroid/patterngallery/presenters/GalleryImageAdapter.java
@@ -11,7 +11,6 @@ import android.support.v4.util.Pair;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import com.anthropicandroid.patterngallery.R;
 import com.anthropicandroid.patterngallery.entities.ui.SVGItemViewModel;
 import com.anthropicandroid.patterngallery.routers.gallery.GalleryActivityComponent;
 
