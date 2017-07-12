72ee7b36984a3d163aaa0dd8b97c4b437513104f
Andrew Brin <anthropic.android@gmail.com>
2017-07-11 21:45:51 -0700

    paths drawn on the view

--- app/src/main/java/com/anthropicandroid/patterngallery/presenters/SVGDetailAdapter.java
+++ app/src/main/java/com/anthropicandroid/patterngallery/presenters/SVGDetailAdapter.java
@@ -8,6 +8,9 @@ import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.graphics.RectF;
@@ -17,6 +20,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 
 import com.anthropicandroid.patterngallery.R;
@@ -25,6 +29,7 @@ import com.anthropicandroid.patterngallery.entities.interactions.PatternMetaData
 import com.anthropicandroid.patterngallery.entities.ui.SVGItemViewModel;
 import com.anthropicandroid.patterngallery.routers.gallery.GalleryActivityComponent;
 import com.anthropicandroid.patterngallery.entities.ui.RawBitmapMeasurement;
+import com.anthropicandroid.patterngallery.view.PatternDetailView;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -37,9 +42,17 @@ public class SVGDetailAdapter {
     @BindingAdapter("svgDetailPath")
     public static void setSVGDetailPath(
             final GalleryActivityComponent galleryActivityComponent,
-            final ImageView imageView,
+            final PatternDetailView imageView,
             final Path path
     ) {
+        Paint paint = new Paint() {{
+            setAntiAlias(false);
+            setStyle(Style.STROKE);
+            setStrokeWidth(4f);
+            setColor(Color.WHITE);
+            setAlpha(255);
+        }};
+
         //  Keeping this RectF instantiation in the method, as it would be a static field
         RectF pathBounds = new RectF();
         CoordinatorLayout parent = (CoordinatorLayout) imageView.getParent();
@@ -57,5 +70,17 @@ public class SVGDetailAdapter {
         params.gravity = Gravity.CENTER_HORIZONTAL;
         params.setMargins(200, rootAppBarLayout.getBottom() + chordOutline.getTop(), 200, 200);
         imageView.setLayoutParams(params);
+
+        imageView.setPath(path);
+
+//        dd
+//        if (!pathBounds.isEmpty()) {
+//            Canvas canvas = new Canvas(Bitmap.createBitmap((int) pathBounds.width(), (int) pathBounds.height(), Bitmap.Config.ARGB_8888));
+//            canvas.save();
+//            canvas.drawPath(path, paint);
+//            canvas.translate(imageView.getLeft(), imageView.getTop());
+//            imageView.draw(canvas);
+//            canvas.restore();
+//        }
     }
 }
