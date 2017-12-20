3f5cd8757bea76eb5fa2d146acc72b25e387dc60
Andrew Brin <anthropic.android@gmail.com>
2017-12-19 17:55:15 -0800

    whitespace

--- app/src/main/java/com/anthropicandroid/patterngallery/presenters/BottomNavActionHandlers.java
+++ app/src/main/java/com/anthropicandroid/patterngallery/presenters/BottomNavActionHandlers.java
@@ -26,22 +26,24 @@ import rx.schedulers.Schedulers;
 /*
  * Created by Andrew Brin on 7/17/2016.
  */
-public class BottomNavActionHandlers {
+public class BottomNavActionHandlers
+{
 
     public BottomNavActionHandlers(Application context) {
 
     }
 
     public void writePatternToDevice(View view) {
-        LayoutActivityGalleryBinding binding = DataBindingUtil.findBinding(view);
-        final PatternDetailView svgDetail = binding.svgDetail;
-        final Button abscissaBox = binding.abscissaBox;
-        final Button ordinalBox = binding.ordinalBox;
+        LayoutActivityGalleryBinding binding     = DataBindingUtil.findBinding(view);
+        final PatternDetailView      svgDetail   = binding.svgDetail;
+        final Button                 abscissaBox = binding.abscissaBox;
+        final Button                 ordinalBox  = binding.ordinalBox;
 
         Path path = binding.getDetailPath();
         Observable
                 .just(path)
-                .filter(new Func1<Path, Boolean>() {
+                .filter(new Func1<Path, Boolean>()
+                {
                     @Override
                     public Boolean call(Path path) {
                         boolean empty = path.isEmpty();
@@ -49,21 +51,24 @@ public class BottomNavActionHandlers {
                         return !empty;
                     }
                 })
-                .flatMap(new Func1<Path, Observable<float[]>>() {
+                .flatMap(new Func1<Path, Observable<float[]>>()
+                {
                     @Override
                     public Observable<float[]> call(Path path) {
                         final PathMeasure pathMeasure = new PathMeasure(path, false);
-                        final float length = pathMeasure.getLength();
+                        final float       length      = pathMeasure.getLength();
 
                         return Observable
                                 .interval(100, TimeUnit.MILLISECONDS)
-                                .takeUntil(new Func1<Long, Boolean>() {
+                                .takeUntil(new Func1<Long, Boolean>()
+                                {
                                     @Override
                                     public Boolean call(Long count) {
                                         return count > length;
                                     }
                                 })
-                                .map(new Func1<Long, float[]>() {
+                                .map(new Func1<Long, float[]>()
+                                {
                                     @Override
                                     public float[] call(Long distance) {
                                         float[] pos = new float[2];
@@ -79,7 +84,8 @@ public class BottomNavActionHandlers {
                 })
                 .subscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<float[]>() {
+                .subscribe(new Subscriber<float[]>()
+                {
                     @Override
                     public void onCompleted() {
                         Log.i(getClass().getSimpleName(), "write completed");
@@ -87,35 +93,50 @@ public class BottomNavActionHandlers {
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.e(getClass().getSimpleName(), "error while writing pattern: " + e.getMessage());
+                        Log.e(
+                                getClass().getSimpleName(),
+                                "error while writing pattern: " + e.getMessage());
                         e.printStackTrace();
                     }
 
                     @Override
                     public void onNext(float[] floats) {
-                        abscissaBox.setText(new BigDecimal(floats[0]).setScale(2, BigDecimal.ROUND_DOWN).toString());
-                        ordinalBox.setText(new BigDecimal(floats[1]).setScale(2, BigDecimal.ROUND_DOWN).toString());
+                        abscissaBox.setText(new BigDecimal(
+                                floats[0]).setScale(
+                                2,
+                                BigDecimal.ROUND_DOWN)
+                                          .toString());
+                        ordinalBox.setText(new BigDecimal(
+                                floats[1]).setScale(
+                                2,
+                                BigDecimal.ROUND_DOWN)
+                                          .toString());
                         svgDetail.showSparkAt(floats[0], floats[1]);
                     }
                 });
     }
 
-    public void selectTool(View view){
-        Toast.makeText(view.getContext(), "tool selection not yet implemented", Toast.LENGTH_SHORT).show();
+    public void selectTool(View view) {
+        Toast.makeText(view.getContext(), "tool selection not yet implemented", Toast.LENGTH_SHORT)
+             .show();
     }
 
-    public void reverseDirection(View view){
-        Toast.makeText(view.getContext(), "direction reversal not yet implemented", Toast.LENGTH_SHORT).show();
+    public void reverseDirection(View view) {
+        Toast.makeText(
+                view.getContext(),
+                "direction reversal not yet implemented",
+                Toast.LENGTH_SHORT)
+             .show();
     }
 
     public boolean valueIncreaser(
             View view,
             MotionEvent motionEvent
-    ){
-        int                          action = MotionEventCompat.getActionMasked(motionEvent);
+    ) {
+        int                          action  = MotionEventCompat.getActionMasked(motionEvent);
         LayoutActivityGalleryBinding binding = DataBindingUtil.findBinding(view);
 
-        switch (action){
+        switch (action) {
             case MotionEvent.ACTION_DOWN:
                 binding.sliderControlLayout.setVisibility(View.VISIBLE);
                 return true;
@@ -125,7 +146,8 @@ public class BottomNavActionHandlers {
             case MotionEvent.ACTION_CANCEL:
                 binding.sliderControlLayout.setVisibility(View.GONE);
                 return true;
-            default: return false;
+            default:
+                return false;
         }
     }
 
